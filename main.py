60bf4bc616c6ad482e616102db3e7f13d74cbddb
Daliana-Solis <dsolisso@uci.edu>
2023-09-19 17:48:11 -0500

    Return temperature for stations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 
 #__name__ -->
@@ -14,9 +15,14 @@ def home():
     return render_template("home.html")
 
 #any value user enters for station and date
-@app.route("api/vi/<station>/<date>")
+@app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
+    file_name = "data_small/TG_STAID" + str(station).zfill(6)+".txt"
+
+    df = pd.read_csv(file_name, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]['   TG'].squeeze()/10
+
+
     return {"station": station,
             "date": date,
             "temperature": temperature}
